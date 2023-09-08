e5bab52f5bb2858b3f26459dc5f7960a9e6ec728
BrianKani <kipsangkangogo26@gmail.com>
2023-09-08 02:14:31 -0700

    Fabric script (based on the file 2-do_deploy_web_static.py)

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,13 +1,36 @@
 #!/usr/bin/python3
-""" Function that compress a folder """
+""" Function that deploys """
 from datetime import datetime
 from fabric.api import *
-import shlex
 import os
+import shlex
 
 
 env.hosts = ['35.231.33.237', '34.74.155.163']
 env.user = "ubuntu"
+
+
+def deploy():
+    """ DEPLOYS """
+    try:
+        archive_path = do_pack()
+    except:
+        return False
+
+    return do_deploy(archive_path)
+
+
+def do_pack():
+    try:
+        if not os.path.exists("versions"):
+            local('mkdir versions')
+        t = datetime.now()
+        f = "%Y%m%d%H%M%S"
+        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        return archive_path
+    except:
+        return None
 
 
 def do_deploy(archive_path):
